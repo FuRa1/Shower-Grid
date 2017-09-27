58ed4fe6c906e60011cccb27910365f38066dc47
fura1 <grevtsov.b@protonmail.com>
2017-09-27 12:28:38 +0300

    autofill minmax2

--- shower/my.js
+++ shower/my.js
@@ -1,7 +1,6 @@
 $( document ).ready(function() {
     let $container = $('.grid-container--6-auto-fill');
     let $input = $('#containerSize');
-    let $containerWidth = $container.css('width');
     $input.on('change', function () {
         let value = parseFloat($( this ).val())*100;
         if(value > 400 && value < 900){
